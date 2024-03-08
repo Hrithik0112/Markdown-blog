2615113c2ce6b80e2a854e90e54a6e11dd9c4047
Hrithik <slumdog4321@gmail.com>
2024-03-09 00:06:31 +0530

    Added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="font-bold">Welcome To Reality</h1>
+      <Button>Click Me!</Button>
     </div>
   );
 }
