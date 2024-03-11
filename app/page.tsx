83d1590a1becf4fb0b06aeca162ebed777caa1bc
Hrithik <slumdog4321@gmail.com>
2024-03-11 23:53:40 +0530

    added site header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="font-bold">Welcome To Reality</h1>
-      <Button>Click Me!</Button>
-    </div>
-  );
+  return <section>Nextjs 14 blog using velite, tailwind and shadcn</section>;
 }
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "DevBlog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "JollyCoding",
+  links: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/",
+    personalSite: "https://example.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
