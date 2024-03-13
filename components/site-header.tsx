cc1b01dbe668c9eaeeea2d759961266baa31ad51
Hrithik <slumdog4321@gmail.com>
2024-03-13 23:58:27 +0530

    added mobile navigation

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./icons";
 import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   return (
@@ -13,17 +14,28 @@ export function SiteHeader() {
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center">
             <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
-              <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
+              <div
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  "w-10 px-0 hidden sm:inline-block"
+                )}
+              >
                 <Icons.gitHub className="h-4 w-4" />
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
             <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
-              <div className={cn(buttonVariants({ variant: "ghost" }), "w-10 px-0")}>
+              <div
+                className={cn(
+                  buttonVariants({ variant: "ghost" }),
+                  "w-10 px-0 hidden sm:inline-block"
+                )}
+              >
                 <Icons.twitter className="h-4 w-4" />
                 <span className="sr-only">Twitter</span>
               </div>
             </Link>
+            <MobileNav />
           </nav>
         </div>
       </div>
